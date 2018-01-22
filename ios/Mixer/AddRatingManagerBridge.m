d646baf6af34a60317fe4c1107629f05fc32e7ff
ulrichsinn <uli@addtothenoise.com>
2018-01-22 13:44:02 -0800

    delete old files

--- ios/Mixer/AddRatingManagerBridge.m
+++
@@ -1,8 +0,0 @@
-#import <React/RCTBridgeModule.h>
-
-@interface RCT_EXTERN_MODULE(AddRatingManager, NSObject)
-
-RCT_EXTERN_METHOD(dismissPresentedViewController:(nonnull NSNumber *)reactTag)
-
-RCT_EXTERN_METHOD(save:(nonnull NSNumber *)reactTag rating:(NSInteger *)rating forIdentifier:(NSInteger *)forIdentifier)
-@end
--- js/index.ios.js
+++ js/index.ios.js
@@ -1,10 +1,13 @@
 'use strict';
 // 1
-import React from 'react';
+import React, { Component } from 'react';
 import ReactNative, {
     AppRegistry,
 } from 'react-native';
 
-import AddRatingApp from './AddRatingApp';
 
-AppRegistry.registerComponent('AddRatingApp', () => AddRatingApp);
+import LandingScreen_1 from './containers/LandingScreen_1'
+import LandingScreen_2 from './containers/LandingScreen_2'
+
+AppRegistry.registerComponent('LandingScreen_1', () => LandingScreen_1);
+AppRegistry.registerComponent('LandingScreen_2', () => LandingScreen_2);
